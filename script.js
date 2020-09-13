aa8db5c71fb6f305dc8758703faabb22c114ef0e
Steven Bong <stevenseal99@yahoo.com>
2020-09-12 19:45:11 -0700

    Start working on laying out the HTML elements and stating the pseudo code on both HTML and JS files. Did research on how to progress this HW

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+// Generate the quiz questions
 const quizQuestion = [
     {
         question1 = ""
     }
 ]
+
+// Declare the variables using the getElementById
+
+// State the current position
+
+// Create function to store the quiz into the website
+
+// 
