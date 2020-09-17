deb443aac49da1c88c79904f29bee95ef2c437a6
Steven Bong <stevenseal99@yahoo.com>
2020-09-16 18:29:16 -0700

    Asked tutor and Joanna about the HTML and JS file. Changed the HTML form layout for the result page. Re-worked the JS file including the iterate function and checking answer whether it's right or wrong. Add list of people to the result page.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # QuizGame
-Week3HW#1
+Link to github repo:
+Link to the website:
+
+Week 3 HW #1 Quiz Game App
+
+The overall scope of this HW is to enhance the students ability in utilizing Javascript to build a simple quiz application along with several minimum requirements. This HW also utilizes HTML and CSS to build this Quiz App. For this HW, students need to make all the files from the scratch and use their own creativity to build the whole Quiz App. There are several steps that I took to build this Quiz App, which listed as below:
+1.  Started by making the HTML file on how to layout the quiz
+2.  Style the HTML file using the CSS file 
+3.  Made the JS file to do all the user interfacing commands
+4.  Then, in JS file, first declare the variables to get the elementbyId form HTML
+5.  Create several function for each task that you would like to work i.e. setTimer() function, selectedAnswer() function, storedQuiz() function, etc.
+6.  Add the button eventlistener command for the user to be able to work the quiz, set it to the next question, and direct them to the result page
+7.  Made another form for the user to put their name into the list and store it to the local storage, so the input will not be deleted every time the user start the quiz again
+
+Screenshot of what I made for this Quiz App listed below:
+
--- script.js
+++ script.js
@@ -6,36 +6,43 @@ const quizQuestion = [
         answer2: "answer 1 and 3 are corret",
         answer3: "const yourAge = 30",
         answer4: "country = USA",
-        correctAns: "answer2",
+        correctAns: "answer 1 and 3 are corret",
     }, {
         questions: "What's the command for inserting a string into an array?",
         answer1: ".forEeach()",
         answer2: ".shift()",
         answer3: "myFunction()",
         answer4: ".push()",
-        correctAns: "answer4",
+        correctAns: ".push()",
     }, {
         questions: "How to log your value to browser console?",
         answer1: "cnsl.lg()",
         answer2: "console.println()",
         answer3: "console.log()",
         answer4: "const console = log()",
-        correctAns: "answer3",
+        correctAns: "console.log()",
     }, {
         questions: "What's the result of var x = (1 + 1 * 0) ? ",
         answer1: "1",
         answer2: "0",
         answer3: "2",
         answer4: "Undefined",
-        correctAns: "answer1",
+        correctAns: "1",
     }, {
-        questions: "How to conver letters to upper case?",
+        questions: "How to convert letters to upper case?",
         answer1: ".toUpperCase()",
         answer2: "var x = toUpperCase()",
         answer3: "changeToUpperCase()",
         answer4: ".shiftToUpperCase()",
-        correctAns: "answer1",
-    },
+        correctAns: ".toUpperCase()",
+    }, {
+        questions: "Loading",
+        answer1: "Loading",
+        answer2: "Loading",
+        answer3: "Loading",
+        answer4: "Loading",
+        correctAns: "",
+    }
 ];
 
 // Declare the variables from HTML using the getElementById
@@ -45,21 +52,27 @@ var option1 = document.getElementById("choice1");
 var option2 = document.getElementById("choice2");
 var option3 = document.getElementById("choice3");
 var option4 = document.getElementById("choice4");
-var submitButton = document.getElementById("submit");
-var container = document.getElementById(".container");
 var timer = document.getElementById("timer");
-var result = document.getElementById("result");
 var entireAnswer = document.querySelectorAll(".answerOption");
 var listPeople = document.querySelector("#listpeople");
 var addButton = document.querySelector("#addbutton");
-var quizScore = document.getElementById("score");
+var form = document.querySelector(".create-form");
+var questionList = document.querySelector(".question-list");
+var hiding = document.querySelector(".hiding");
+var nameEl = document.querySelector("#nameinput")
 
 // State the start position
+let previousChoice = "";
 let currentState = 0;
-// let quizScore = 0;
+let quizScore = 0;
 var setTime = 60;
 var people = [{name: "Steven"}];
-// let currentAnswer = none;
+
+// Add an event listener to the buttons and connect it with the iterate function
+option1.addEventListener("click", iterate);
+option2.addEventListener("click", iterate);
+option3.addEventListener("click", iterate);
+option4.addEventListener("click", iterate);
 
 // Call the quiz function
 storeQuiz();
@@ -68,7 +81,7 @@ storeQuiz();
 function storeQuiz(){
     // Declare variable to load the quiz question followed by the answer
     var loadCurrentQuestion = quizQuestion[currentState];
-
+    
     // Set up the question to work along with the function
     questionOptions.textContent = loadCurrentQuestion.questions;
     option1.textContent = loadCurrentQuestion.answer1;
@@ -77,53 +90,27 @@ function storeQuiz(){
     option4.textContent = loadCurrentQuestion.answer4;
 }
 
-// Create a function to check if the result selected by user right or wrong
-function selectedAnswer(){
-    let checkAnswer = undefined;
-    // Check this loop
-    entireAnswer.forEach(function(entireAnswer){
-        if(entireAnswer.checked){
-            checkAnswer = entireAnswer.id;
-        }
-    });
-    return checkAnswer;
-}
+// Create a function to state the result
+function iterate(){
+    // Create variable of correct answer
+    var correctAnswer = quizQuestion[currentState].correctAns;
+    var lastAnswer = this.textContent;
 
-// Clear the selected answer function
-function clearAnswer(){
-    // Also check this loop
-    entireAnswer.forEach(function(entireAnswer){
-        entireAnswer.checked = false;
-    })
-}
+    // Create a conditional statement
+    if(correctAnswer === undefined){
 
-// Add event listener to the submit button
-submitButton.addEventListener("click", function(){
-    // Check the answer
-    var answerChoices = selectedAnswer();
-
-    if(answerChoices){
-        // Check variables
-        if(answerChoices === quizQuestion[currentState].correctAns){
-            quizScore.textContent = quizScore;
-            // score = score + 20;
-        }
-        currentState++;
-        if(currentState < quizQuestion.length){
-            storeQuiz();
-        } else if (currentState === quizQuestion.length - 1){
-            quizApp.innerHTML = `<button>Submit</button>`;
-        } else {
-            quizApp.innerHTML = `<h2>You're done with the Quiz!!! Here's the result:</h2>
-            <form>
-            <ul id="listpeople"></ul>
-                <input type="text" id="nameinput" placeholder="Put your name here">
-                <button id="addbutton">Add to the Honorable Mentions</button>
-            </form>
-            <button onclick="location.reload()">Reload</button>`;
-        }
+    } else if(lastAnswer === correctAnswer){
+        quizScore += 20;
+        // Put a text for the correct answers
+    } else {
+        setTime -= 5;
     }
-})
+    currentState++;
+    storeQuiz();
+
+    // Store the quizScore in local storage
+    localStorage.setItem("key", quizScore);
+}
 
 // Set the timer countdown function
 function setTimerCountdown(){
@@ -132,34 +119,46 @@ function setTimerCountdown(){
         timer.textContent = "Time: " + setTime;
 
         // Conditional statement if time = 0
-        if(setTime === 0){
+        if(setTime === 0 || currentState >= 5){
             clearInterval(timeCount);
-            // getResult();
+            quizScore += setTime;
+            form.style.display = "block";
+            hiding.style.display = "none";
         }
     }, 1000)
 }
-
-// Set the result page to show after last question has been answered
 
 // On the result page, add list of person including the scores they achieved from Activity 19
 function listOfPerson(event){
     event.preventDefault();
     // Create variable to add name to 
-    var addName = nameList.value;
+    var addName = nameEl.value;
+    
+    // Create if statement to check if the user put a name or not
+    if(!addName){
+        return alert("Please put your initials!")
+    }
+
+    // Create list of people who worked on the quiz
     var li = document.createElement("li");
     li.id = people.length;
-    li.innerHTML = name;
-    people.push({name: name});
+    li.innerHTML = addName + " scoring " + quizScore + " points in the quiz";
+    people.push({addName: addName});
     listPeople.appendChild(li);
-
-    // Create if statement to check if the user put a name or not
-    if(!name){
-        return
-    }
+    
+    // Store name in local storage
+    localStorage.setItem("key", addName);
 }
 
+// Store data in local storage
+localStorage.setItem("key", quizScore);
+
+// localStorage.setItem("key", quizScore); // key would be the text content of the input box on top of my submit button and quiz score will be the text content of the box on top of the submit button + quiz score
+    // localStorage.getItem()
+    // getItem()
+
 // Add the event listener to the buttons
-// addButton.addEventListener("click", listOfPerson());
+addButton.addEventListener("click", listOfPerson);
 
 // Call the setTimerCountdown function to display the timer
 setTimerCountdown();
